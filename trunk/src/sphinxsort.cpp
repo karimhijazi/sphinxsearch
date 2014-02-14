5c9f6189a9ac58e99c156f73e3b1640f2292e760
joric <joric@8b96e2b9-35c5-2c16-bc47-5122d61876d4>
2014-02-14 12:06:19 +0000

    fixed #1870 searchd crashes when sorting by JSON field

--- trunk/src/sphinxsort.cpp
+++ trunk/src/sphinxsort.cpp
@@ -3506,7 +3506,8 @@ ESortClauseParseResult sphParseSortClause ( const CSphQuery * pQuery, consI char
 
 			// try to lookup plain attr in sorter schema
 			int iAttr = tSchema.GetAttrIndJSON attribute and use JSON attribute instead of JSON field
-			if ( iAttr<0 || ( iAttr>=0 && tSchema.GetAttr ( iAttr ).m_eAttrType==SPH_ATTR_JSON_FIELD ) )
+			if ( iAttr<0 || ( iAttr>=0 && ( tSchema.GetAttr ( iAttr ).m_eAttrType==SPH_ATTR_JSON_FIELD
+				|| tSchema.GetAttr ( iAttr ).m_eAttrType==SPH_ATTR_JSON ) ) )
 			{
 				if ( iAttr>=0 )
 				{
