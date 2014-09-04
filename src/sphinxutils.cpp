karimhijazi/sphinxsearch

commit 6ccd236810d8ff579a01ee6bfae35e94805f79a4

removing vptr table from CSphString class
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@4796 8b96e2b9-35c5-2c16-bc47-5122d61876d4

src/sphinxutils.cpp
@@ -1390,7 +1390,7 @@ bool sphConfIndex ( const CSphConfigSection & hIndex, CSphIndexSettings & tSetti
 	// hit-less indices
 	if ( hIndex("hitless_words") )
 	{
-		const CSphString & sValue = hIndex["hitless_words"];
+		const CSphString & sValue = hIndex["hitless_words"].strval();
 		if ( sValue=="all" )
 		{
 			tSettings.m_eHitless = SPH_HITLESS_ALL;
@@ -1409,7 +1409,7 @@ bool sphConfIndex ( const CSphConfigSection & hIndex, CSphIndexSettings & tSetti
 	tSettings.m_eBigramIndex = SPH_BIGRAM_NONE;
 	if ( hIndex("bigram_index") )
 	{
-		CSphString & s = hIndex["bigram_index"];
+		CSphString s = hIndex["bigram_index"].strval();
 		s.ToLower();
 		if ( s=="all" )
 			tSettings.m_eBigramIndex = SPH_BIGRAM_ALL;
@@ -2256,9 +2256,9 @@ void sphCheckDuplicatePaths ( const CSphConfig & hConf )
 		if ( hIndex ( "path" ) )
 		{
 			const CSphString & sIndex = hConf["index"].IterateGetKey ();
-			if ( hPaths ( hIndex["path"] ) )
-				sphDie ( "duplicate paths: index '%s' has the same path as '%s'.\n", sIndex.cstr(), hPaths[hIndex["path"]].cstr() );
-			hPaths.Add ( sIndex, hIndex["path"] );
+			if ( hPaths ( hIndex["path"].strval() ) )
+				sphDie ( "duplicate paths: index '%s' has the same path as '%s'.\n", sIndex.cstr(), hPaths[hIndex["path"].strval()].cstr() );
+			hPaths.Add ( sIndex, hIndex["path"].strval() );
 		}
 	}
 }
@@ -2283,7 +2283,7 @@ void sphConfigureCommon ( const CSphConfig & hConf )
 
 	if ( hCommon("on_json_attr_error") )
 	{
-		const CSphString & sVal = hCommon["on_json_attr_error"];
+		const CSphString & sVal = hCommon["on_json_attr_error"].strval();
 		if ( sVal=="ignore_attr" )
 			bJsonStrict = false;
 		else if ( sVal=="fail_index" )
@@ -2294,7 +2294,7 @@ void sphConfigureCommon ( const CSphConfig & hConf )
 
 	if ( hCommon("json_autoconv_keynames") )
 	{
-		const CSphString & sVal = hCommon["json_autoconv_keynames"];
+		const CSphString & sVal = hCommon["json_autoconv_keynames"].strval();
 		if ( sVal=="lowercase" )
 			bJsonKeynamesToLowercase = true;
 		else

src/indexer.cpp
@@ -384,7 +384,7 @@ bool ParseMultiAttr ( const char * sBuf, CSphColumnInfo & tAttr, const char * sS
 // get string
 #define LOC_GETS(_arg,_key) \
 	if ( hSource.Exists(_key) ) \
-		_arg = hSource[_key];
+		_arg = hSource[_key].strval();
 
 // get int
 #define LOC_GETI(_arg,_key) \
@@ -917,7 +917,7 @@ bool DoIndex ( const CSphConfigSection & hIndex, const char * sIndexName,
 	bool bPlain = true;
 	if ( hIndex("type") )
 	{
-		const CSphString & sType = hIndex["type"];
+		const CSphString & sType = hIndex["type"].strval();
 		bPlain = ( sType=="plain" );
 
 		if ( sType!="plain" && sType!="distributed" && sType!="rt" && sType!="template" )
@@ -1840,7 +1840,7 @@ int main ( int argc, char ** argv )
 
 		if ( hIndexer("on_file_field_error") )
 		{
-			const CSphString & sVal = hIndexer["on_file_field_error"];
+			const CSphString & sVal = hIndexer["on_file_field_error"].strval();
 			if ( sVal=="ignore_field" )
 				g_eOnFileFieldError = FFE_IGNORE_FIELD;
 			else if ( sVal=="skip_document" )
@@ -1856,7 +1856,7 @@ int main ( int argc, char ** argv )
 		bool bJsonKeynamesToLowercase = false;
 		if ( hIndexer("on_json_attr_error") )
 		{
-			const CSphString & sVal = hIndexer["on_json_attr_error"];
+			const CSphString & sVal = hIndexer["on_json_attr_error"].strval();
 			if ( sVal=="ignore_attr" )
 				bJsonStrict = false;
 			else if ( sVal=="fail_index" )
@@ -1867,7 +1867,7 @@ int main ( int argc, char ** argv )
 
 		if ( hIndexer("json_autoconv_keynames") )
 		{
-			const CSphString & sVal = hIndexer["json_autoconv_keynames"];
+			const CSphString & sVal = hIndexer["json_autoconv_keynames"].strval();
 			if ( sVal=="lowercase" )
 				bJsonKeynamesToLowercase = true;
 			else

src/indextool.cpp
@@ -992,11 +992,11 @@ int main ( int argc, char ** argv )
 		while ( hIndexes.IterateNext() )
 		{
 			const CSphConfigSection & tIndex = hIndexes.IterateGet();
-			const CSphString * pPath = tIndex ( "path" );
+			const CSphVariant * pPath = tIndex ( "path" );
 			if ( !pPath )
 				continue;
 
-			const CSphString * pType = tIndex ( "type" );
+			const CSphVariant * pType = tIndex ( "type" );
 			if ( pType && ( *pType=="rt" || *pType=="distributed" ) )
 				continue;
 
@@ -1091,7 +1091,7 @@ int main ( int argc, char ** argv )
 		{
 			CSphIndexSettings tSettings = pIndex->GetSettings();
 
-			const CSphString & sValue = hConf["index"][sIndex]["hitless_words"];
+			const CSphString & sValue = hConf["index"][sIndex]["hitless_words"].strval();
 			if ( sValue=="all" )
 			{
 				tSettings.m_eHitless = SPH_HITLESS_ALL;

src/searchd.cpp
@@ -19953,7 +19953,7 @@ bool ConfigureAgent ( MetaAgentDesc_t & tAgent, const CSphVariant * pAgent, cons
 						szIndexName, pAgent->cstr(), p );
 					return false;
 				}
-				CSphString sSub = pAgent->SubString ( pAnchor-pAgent->cstr(), p-pAnchor );
+				CSphString sSub = pAgent->strval().SubString ( pAnchor-pAgent->cstr(), p-pAnchor );
 				if ( sSub.cstr()[0]=='/' )
 				{
 #if USE_WINDOWS
@@ -20041,7 +20041,7 @@ bool ConfigureAgent ( MetaAgentDesc_t & tAgent, const CSphVariant * pAgent, cons
 				if ( sphIsAlpha(*p) || isspace(*p) || *p==',' )
 					break;
 
-				CSphString sIndexes = pAgent->SubString ( pAnchor-pAgent->cstr(), p-pAnchor );
+				CSphString sIndexes = pAgent->strval().SubString ( pAnchor-pAgent->cstr(), p-pAnchor );
 
 				if ( *p && *p!='|' )
 				{
@@ -20342,7 +20342,7 @@ ESphAddIndex AddIndex ( const char * szIndexName, const CSphConfigSection & hInd
 			sphWarning ( "WARNING: index '%s': prefix_fields and infix_fields has no effect with dict=keywords, ignoring\n", szIndexName );
 		tIdx.m_pIndex = sphCreateIndexRT ( tSchema, szIndexName, iRamSize, hIndex["path"].cstr(), bWordDict );
 		tIdx.m_bEnabled = false;
-		tIdx.m_sIndexPath = hIndex["path"];
+		tIdx.m_sIndexPath = hIndex["path"].strval();
 		tIdx.m_bRT = true;
 
 		ConfigureLocalIndex ( tIdx, hIndex );
@@ -20397,7 +20397,7 @@ ESphAddIndex AddIndex ( const char * szIndexName, const CSphConfigSection & hInd
 		ConfigureLocalIndex ( tIdx, hIndex );
 
 		// try to create index
-		tIdx.m_sIndexPath = hIndex["path"];
+		tIdx.m_sIndexPath = hIndex["path"].strval();
 		PreCreatePlainIndex ( tIdx, szIndexName );
 		tIdx.m_pIndex->SetCacheSize ( g_iMaxCachedDocs, g_iMaxCachedHits );
 		CSphIndexStatus tStatus;
@@ -22838,7 +22838,7 @@ int WINAPI ServiceMain ( int argc, char **argv )
 	{
 		// listen directives in configuration file
 		for ( CSphVariant * v = hSearchd("listen"); v; v = v->m_pNext )
-			AddListener ( *v );
+			AddListener ( v->strval() );
 
 		// default is to listen on our two ports
 		if ( !g_dListeners.GetLength() )

src/sphinx.cpp
@@ -28036,7 +28036,7 @@ struct CSphSchemaConfigurator
 	{
 		for ( const CSphVariant * pCur = pHead; pCur; pCur= pCur->m_pNext )
 		{
-			CSphColumnInfo tCol ( pCur->cstr(), eAttrType );
+			CSphColumnInfo tCol ( pCur->strval().cstr(), eAttrType );
 			char * pColon = strchr ( const_cast<char*> ( tCol.m_sName.cstr() ), ':' );
 			if ( pColon )
 			{
@@ -28082,7 +28082,7 @@ struct CSphSchemaConfigurator
 	{
 		for ( const CSphVariant * pCur = pHead; pCur; pCur= pCur->m_pNext )
 		{
-			const char * sFieldName = pCur->cstr();
+			const char * sFieldName = pCur->strval().cstr();
 
 			bool bFound = false;
 			for ( int i = 0; i < tSchema.m_dFields.GetLength () && !bFound; i++ )
@@ -30148,7 +30148,7 @@ bool CSphSource_BaseSV::Setup ( const CSphConfigSection & hSource, FILE * pPipe,
 		const CSphVariant * pVal = &hSource.IterateGet();
 		while ( pVal )
 		{
-			sColumn = *pVal;
+			sColumn = pVal->strval();
 			// uint attribute might have bit count that should by cut off from name
 			const char * pColon = strchr ( sColumn.cstr(), ':' );
 			if ( pColon )

src/sphinxstd.h
@@ -1648,7 +1648,7 @@ public:
 		*this = rhs;
 	}
 
-	virtual ~CSphString ()
+	~CSphString ()
 	{
 		SafeDeleteArray ( m_sValue );
 	}
@@ -2046,9 +2046,10 @@ private:
 
 /// immutable string/int/float variant list proxy
 /// used in config parsing
-struct CSphVariant : public CSphString
+struct CSphVariant
 {
 protected:
+	CSphString		m_sValue;
 	int				m_iValue;
 	int64_t			m_i64Value;
 	float			m_fValue;
@@ -2062,8 +2063,7 @@ public:
 public:
 	/// default ctor
 	CSphVariant ()
-		: CSphString ()
-		, m_iValue ( 0 )
+		: m_iValue ( 0 )
 		, m_i64Value ( 0 )
 		, m_fValue ( 0.0f )
 		, m_pNext ( NULL )
@@ -2074,10 +2074,10 @@ public:
 
 	/// ctor from C string
 	CSphVariant ( const char * sString, int iTag )
-		: CSphString ( sString )
-		, m_iValue ( m_sValue ? atoi ( m_sValue ) : 0 )
-		, m_i64Value ( m_sValue ? (int64_t)strtoull ( m_sValue, NULL, 10 ) : 0 )
-		, m_fValue ( m_sValue ? (float)atof ( m_sValue ) : 0.0f )
+		: m_sValue ( sString )
+		, m_iValue ( sString ? atoi ( sString ) : 0 )
+		, m_i64Value ( sString ? (int64_t)strtoull ( sString, NULL, 10 ) : 0 )
+		, m_fValue ( sString ? (float)atof ( sString ) : 0.0f )
 		, m_pNext ( NULL )
 		, m_bTag ( false )
 		, m_iTag ( iTag )
@@ -2086,39 +2086,23 @@ public:
 
 	/// copy ctor
 	CSphVariant ( const CSphVariant & rhs )
-		: CSphString ()
-		, m_iValue ( 0 )
-		, m_i64Value ( 0 )
-		, m_fValue ( 0.0f )
-		, m_pNext ( NULL )
 	{
 		*this = rhs;
 	}
 
 	/// default dtor
 	/// WARNING: automatically frees linked items!
-	virtual ~CSphVariant ()
+	~CSphVariant ()
 	{
 		SafeDelete ( m_pNext );
 	}
 
-	/// int value getter
-	int intval () const
-	{
-		return m_iValue;
-	}
+	const char * cstr() const { return m_sValue.cstr(); }
 
-	/// int64_t value getter
-	int64_t int64val () const
-	{
-		return m_i64Value;
-	}
-
-	/// float value getter
-	float floatval () const
-	{
-		return m_fValue;
-	}
+	const CSphString & strval () const { return m_sValue; }
+	int intval () const	{ return m_iValue; }
+	int64_t int64val () const { return m_i64Value; }
+	float floatval () const	{ return m_fValue; }
 
 	/// default copy operator
 	const CSphVariant & operator = ( const CSphVariant & rhs )
@@ -2127,7 +2111,7 @@ public:
 		if ( rhs.m_pNext )
 			m_pNext = new CSphVariant ( *rhs.m_pNext );
 
-		CSphString::operator = ( rhs );
+		m_sValue = rhs.m_sValue;
 		m_iValue = rhs.m_iValue;
 		m_i64Value = rhs.m_i64Value;
 		m_fValue = rhs.m_fValue;
@@ -2136,6 +2120,9 @@ public:
 
 		return *this;
 	}
+
+	bool operator== ( const char * s ) const { return m_sValue==s; }
+	bool operator!= ( const char * s ) const { return m_sValue!=s; }
 };
 
 //////////////////////////////////////////////////////////////////////////

src/sphinxutils.h
@@ -89,7 +89,7 @@ public:
 	const char * GetStr ( const char * sKey, const char * sDefault="" ) const
 	{
 		CSphVariant * pEntry = (*this)( sKey );
-		return pEntry ? pEntry->cstr() : sDefault;
+		return pEntry ? pEntry->strval().cstr() : sDefault;
 	}
 
 	/// get size option (plain int, or with K/M prefix) value by key and default value

commit 20b079c43fb4fd18267184268828cc61461b1017

fixed windows build (broken in r4796)

src/sphinxutils.cpp
@@ -1,5 +1,5 @@
 //
-// $Id: sphinxutils.cpp 4522 2014-01-30 11:00:18Z tomat $
+// $Id$
 //
 
 //
@@ -1183,7 +1183,7 @@ void sphConfDictionary ( const CSphConfigSection & hIndex, CSphDictSettings & tS
 
 		CSphString sPath;
 		if ( sLastSlash )
-			sPath = pWordforms->SubString ( 0, sLastSlash - pWordforms->cstr() + 1 );
+			sPath = pWordforms->strval().SubString ( 0, sLastSlash - pWordforms->cstr() + 1 );
 
 		HANDLE hFind = FindFirstFile ( pWordforms->cstr(), &tFFData );
 		if ( hFind!=INVALID_HANDLE_VALUE )
@@ -2444,5 +2444,5 @@ bool CSphDynamicLibrary::LoadSymbols ( const char **, void ***, int ) { return f
 #endif
 
 //
-// $Id: sphinxutils.cpp 4522 2014-01-30 11:00:18Z tomat $
+// $Id$
 //